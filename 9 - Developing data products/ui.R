d0408fba25cd5ec7714ea35ca4a1c8824f89e566
TingtingZha <40467407+TingtingZha@users.noreply.github.com>
2018-08-31 13:14:35 +0200

    Update ui.R

--- 9 - Developing data products/ui.R
+++ 9 - Developing data products/ui.R
@@ -4,10 +4,10 @@ shinyUI(fluidPage(
    sidebarLayout(
     sidebarPanel(
       sliderInput("mpg", "Miles Per Gallon", min=10, max=34, value=c(15,30), step=0.1),
-                   checkboxGroupInput("cyl", "Number of cylinders", c("4"=4, "6"=6, "8"=8), selected = c(4,6,8)),
-                   sliderInput("disp", "Displacement", min=70, max=480, value=c(70,480), step=10),
-                   sliderInput("hp", "Gross horsepower", min=50, max=340, value=c(80,300), step=10),
-                   checkboxGroupInput("am", "Transmission", c("Automatic"=0, "Manual"=1), selected = c(0,1))
+      checkboxGroupInput("cyl", "Number of cylinders", c("4"=4, "6"=6, "8"=8), selected = c(4,6,8)),
+      sliderInput("disp", "Displacement", min=70, max=480, value=c(70,480), step=5),
+      sliderInput("hp", "Gross horsepower", min=50, max=340, value=c(80,300), step=5),
+      checkboxGroupInput("am", "Transmission", c("Automatic"=0, "Manual"=1), selected = c(0,1))
                    ),
                    mainPanel(
                    tabsetPanel(
